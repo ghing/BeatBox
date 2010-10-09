8509545b5405502cf27fb06ed42eb39f0d1ac6d8
animalmother <shifflett.shane@gmail.com>
2010-10-09 18:43:20 -0500

    hooked up the register html to the views.py, modified core/urls.py to map the urls correctly

--- hello_newsroom/apps/core/urls.py
+++ hello_newsroom/apps/core/urls.py
@@ -6,9 +6,10 @@ from core import views
 urlpatterns = patterns('',
     url(r'^smstest',
         views.test_sms),
+    url(r'^m/register', views.mobile_register),
     url(r'^m', views.mobile_index),
     url(r'^admin', include(admin.site.urls)),
-    url(r'', views.index,name="search"),
+    url(r'', views.mobile_index,name="registersubmit")
 )
 
 
--- hello_newsroom/apps/core/views.py
+++ hello_newsroom/apps/core/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render_to_response
 from django.contrib.gis.geos import *
 from django.contrib.gis.measure import Distance
 from django.contrib.gis.shortcuts import render_to_kml
+from django.http import HttpResponseRedirect 
 
 
 
@@ -26,6 +27,23 @@ def index(request):
 
 def mobile_index(request):
     template_dict = {}
-    
+    print 'hello2'
     return render_to_response('user-screen.html', template_dict)
 
+def mobile_register(request):
+    template_dict = {}
+    if request.method != 'POST': 
+        return render_to_response('register.html', template_dict)
+    else:
+        template_dict = {}
+        fUsername = request.POST.get('fUsername', '')
+        fPass = request.POST.get('fPassword','')
+        fBeatNum = request.POST.get('fBeatNum','')
+        fCellNum = request.POST.get('fCellNum','')
+        print fUsername
+        beat = models.CpdBeats.objects.get(beat_num=fBeatNum)
+        thisuser = models.User.objects.create_user(username=fUsername, password=fPass)
+        beatuser = models.BeatUser(user=thisuser, cpdBeatIntersection=beat, cellNum = fCellNum)
+        beatuser.save()
+
+        return HttpResponseRedirect('/core/m')
