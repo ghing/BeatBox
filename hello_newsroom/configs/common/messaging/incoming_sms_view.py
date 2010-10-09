c7ec282da2758a29bdfb2eb7adb2a48983fdf7d9
Steven Melendez <smelendez@steven-melendezs-macbook.local>
2010-10-09 16:02:58 -0500

    Some changes to SMS code.

--- hello_newsroom/configs/common/messaging/incoming_sms_view.py
+++ hello_newsroom/configs/common/messaging/incoming_sms_view.py
@@ -4,6 +4,8 @@ from django.shortcuts import render_to_response
 from datetime import datetime
 import core.models # check me
 
+REGISTER, INCIDENT, NOTIFY, APPROVE = range(4)
+
 class SMSParseError(Exception):
     # Raised when SMS fails to parse
     def __init__(self, reason):
@@ -11,8 +13,45 @@ class SMSParseError(Exception):
         pass
 
 def parse_sms(msg):
-    """ Parse an SMS message and return a beat and message."""
-    pass
+    """ Parse an SMS message and return a beat and message.
+    Returns type, beat, message.
+    """
+
+    msg = msg.lower()
+
+    # REGISTER
+    if msg.startswith("register"):
+        try:
+            _, beatnum = msg.split(" ")
+            beat = Beat.get(id = beatnum)
+            return (REGISTER, beat, None)
+
+        except: 
+            raise SMSParseError("Usage: register <beat-number>")
+
+    # NOTIFY
+    elif msg.startswith("notify"):
+        try:
+            _, incident = msg.split(" ")
+            return (NOTIFY, _, incident)
+        except: raise SMSParseError("Usage: NOTIFY <incident-number>")
+
+    # INCIDENT
+    else:
+        if re.match("^\d+ ", msg):
+            try: 
+                beatnum, msg = msg.split(" ", 1)
+                beat = Beat.get(id = beatnum)
+            except:
+                raise SMSParseError("Usage: [beat-number] report")
+
+
+            return (INCIDENT, beat, msg)
+            
+
+    
+
+
 def twilio(request):
     """Handle an incoming HTTP request from twilio.
     Request will contain from, to and body parameters.
@@ -20,32 +59,65 @@ def twilio(request):
 
     # Log the SMS request
     timestamp = datetime.now()
-    sms = models.Sms.objects.create(request.from, request.to, request.body, datetime.now)
+    sms = models.Sms.objects.create(request.From, request.to, request.body, datetime.now)
     sms.save()
 
-    # Look up (or create) the user by phone #
-    user, isNewUser = models.User.objects.get_or_create(cell = sms.from)
 
 
     # Parse the SMS
-    try: beat, msgtype, message = parse_sms(sms.body)
+    try: msgtype, beat, message = parse_sms(sms.body)
     except SMSParseError as e:
         # Handle error condition
         return render_to_response('twilio_responses/parseerror.xml', {'reason': e.reason})
 
 
-    if msgtype == INCIDENT:
-    # Log an incident
-
-    
-    # Register user to beat, if they don't currently have a beat
-    # Otherwise, they stay in their existing beat
-
-    if not user.beat:
-        user.beat = beat
+    if msgtype == REGISTER:
+        user = User(beat=beat, cell = request.From)
         user.save()
+        return render_to_response('twilio_response/registrationreceived.xml')
 
-    incident = Incident.objects.create(reportedby = user, beat = beat, msg = message)
+    if msgtype == INCIDENT:
+        # Log an incident
+
+        # Is the user registered?
+        try: user = models.User.objects.get(cell = sms.sender)
+        except: 
+            return render_to_response('twilio_responses/parseerror.xml', {'reason': 'First register by texting register <beat number>'})
+
+        
+
+        if not beat:
+            # Assume the user's in his home beat, unless he specified one
+            beat = user.beat
+
+        incident = Incident(reportedby = user, beat = beat, msg = message)
+        incident.save()
+
+        if beat.user == user:
+            # If the admin submitted the incident, just notify the beat
+            notify_beat(incident)
+            return render_to_response('twilio_responses/beatnotified.xml')
+        else:
+            notify_admin(incident)
+            return render_to_response('twilio_responses/adminnotified.xml')
+
+    elif msgtype == NOTIFY:
+        # The message should be an incident ID
+        beat = user.beat
+        
+        try: 
+            incident = Incident.objects.get(id = int(msg))
+        except:
+            return render_to_response('twilio_responses/parseerror.xml', {'reason': 'Invalid incident: %s' % msg})
+
+        if not beat.user == user:
+            return render_to_response('twilio_responses/parseerror.xml', {'reason': 'You\'re not the beat administrator'})
+
+        return render_to_response('twilio_responses/beatnotified.xml')
+
+
+
+            
 
     
 
